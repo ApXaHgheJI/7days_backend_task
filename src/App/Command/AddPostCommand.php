cc43f765163e747c5c5704dcf9a22cd42a65adc3
andreiursu <apxahgheji@gmail.com>
2024-04-23 18:30:02 +0300

    Task #2

--- src/App/Command/AddPostCommand.php
+++ src/App/Command/AddPostCommand.php
@@ -3,33 +3,35 @@
 namespace App\Command;
 
 use Domain\Post\PostManager;
+use joshtronic\LoremIpsum;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class AddPostCommand extends Command
 {
-    protected static $defaultName = 'app:add-post';
+    protected static   $defaultName = 'app:add-post';
     protected static $defaultDescription = 'Run app:add-post';
 
     private PostManager $postManager;
+    private LoremIpsum $loremIpsum;
 
-    public function __construct(PostManager $postManager, string $name = null)
+    public function __construct(PostManager $postManager, LoremIpsum $loremIpsum, string $name = null)
     {
         parent::__construct($name);
         $this->postManager = $postManager;
+        $this->loremIpsum = $loremIpsum;
     }
 
     protected function configure(): void
     {
-        $this->addArgument('title')
-            ->addArgument('content');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $title = $input->getArgument('title');
-        $content = $input->getArgument('content');
+        $dateTime = new \DateTime('now');
+        $title    = 'Summary ' . $dateTime->format('Y-m-d');
+        $content  = $this->loremIpsum->paragraphs(1);
 
         $this->postManager->addPost($title, $content);
 
